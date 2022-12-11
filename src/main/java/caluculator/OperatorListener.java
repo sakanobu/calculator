03a6563fc69743025b2f514e2b8861d50f5ace38
sakanobu <grapejuice100percent@gmail.com>
2022-12-11 15:58:11 +0900

    0除算の対応、および、分離していた=を繰り返し押下した場合のcalculateWhenEqualRepushedメソッドをcalculateメソッドに統合

--- src/main/java/caluculator/OperatorListener.java
+++ src/main/java/caluculator/OperatorListener.java
@@ -23,14 +23,38 @@ public class OperatorListener implements ActionListener {
     String cmdName = e.getActionCommand();
 
     if (cmdName.equals("=") && resultPanel.isLastOperationByOperatorPushed()) {
-      resultLabel.setText(calculateWhenEqualRepushed());
+      try {
+        resultLabel.setText(calculate());
+      } catch (ArithmeticException ex) {
+        resultLabel.setText("①、0除算です。ACボタンを押してください。");
+        resultPanel.setBeforeNumber("0");
+        resultPanel.setPushedOperator("");
+        resultPanel.setLastOperationByOperatorPushed(false);
+        pushedOperatorLabel.setText("");
+      }
     } else if (cmdName.equals("=")) {
-      resultLabel.setText(calculate());
+      try {
+        resultLabel.setText(calculate());
+      } catch (ArithmeticException ex) {
+        resultLabel.setText("②、0除算です。ACボタンを押してください。");
+        resultPanel.setBeforeNumber("0");
+        resultPanel.setPushedOperator("");
+        resultPanel.setLastOperationByOperatorPushed(false);
+        pushedOperatorLabel.setText("");
+      }
       resultPanel.setBeforeNumber(displayNumber);
       resultPanel.setLastOperationByOperatorPushed(true);
     } else if (!resultPanel.getPushedOperator().equals("")
         && !resultPanel.isLastOperationByOperatorPushed()) {
-      resultLabel.setText(calculate());
+      try {
+        resultLabel.setText(calculate());
+      } catch (ArithmeticException ex) {
+        resultLabel.setText("③、0除算です。ACボタンを押してください。");
+        resultPanel.setBeforeNumber("0");
+        resultPanel.setPushedOperator("");
+        resultPanel.setLastOperationByOperatorPushed(false);
+        pushedOperatorLabel.setText("");
+      }
       resultPanel.setBeforeNumber(resultLabel.getText());
       resultPanel.setPushedOperator(cmdName);
       resultPanel.setLastOperationByOperatorPushed(true);
@@ -47,62 +71,48 @@ public class OperatorListener implements ActionListener {
 
   String calculate() {
     String displayNumber = resultLabel.getText();
-    BigDecimal bigDecimalResult = BigDecimal.ZERO;
 
-    switch (resultPanel.getPushedOperator()) {
-      case "+":
-        bigDecimalResult =
-            new BigDecimal(resultPanel.getBeforeNumber()).add(new BigDecimal(displayNumber));
-        break;
-      case "-":
-        bigDecimalResult =
-            new BigDecimal(resultPanel.getBeforeNumber()).subtract(new BigDecimal(displayNumber));
-        break;
-      case "×":
-        bigDecimalResult =
-            new BigDecimal(resultPanel.getBeforeNumber()).multiply(new BigDecimal(displayNumber));
-        break;
-      case "÷":
-        bigDecimalResult =
-            new BigDecimal(resultPanel.getBeforeNumber()).divide(new BigDecimal(displayNumber), 30,
-                RoundingMode.HALF_UP);
-        break;
-      default:
-        resultLabel.setText("OperatorListenerのswitch文③でdefault節まで到達しました。");
+    if (displayNumber.contains("0除算")) {
+      return displayNumber;
     }
 
-    return bigDecimalResult.stripTrailingZeros().toPlainString();
-  }
-
-  String calculateWhenEqualRepushed() {
-    String displayNumber = resultLabel.getText();
-    BigDecimal bigDecimalResult = BigDecimal.ZERO;
-
-    switch (resultPanel.getPushedOperator()) {
-      case "+":
-        bigDecimalResult =
-            new BigDecimal(resultPanel.getBeforeNumber()).add(new BigDecimal(displayNumber));
-        break;
-      case "-":
-        bigDecimalResult =
-            new BigDecimal(displayNumber).subtract(new BigDecimal(resultPanel.getBeforeNumber()));
-        break;
-      case "×":
-        bigDecimalResult =
-            new BigDecimal(resultPanel.getBeforeNumber()).multiply(new BigDecimal(displayNumber));
-        break;
-      case "÷":
-        bigDecimalResult =
-            new BigDecimal(displayNumber).divide(new BigDecimal(resultPanel.getBeforeNumber()), 30,
-                RoundingMode.HALF_UP);
-        break;
-      case "":
-        resultLabel.setText("pushedOperatorが空文字列にもかかわらずcalculateメソッドが呼ばれました。");
-        break;
-      default:
-        resultLabel.setText("OperatorListenerのswitch文③でdefault節まで到達しました。");
+    if (resultPanel.getPushedOperator().equals("")) {
+      return displayNumber;
     }
 
+    BigDecimal bigDecimalResult = switch (resultPanel.getPushedOperator()) {
+      case "+" -> new BigDecimal(resultPanel.getBeforeNumber()).add(new BigDecimal(displayNumber));
+      case "-" -> {
+        if (resultPanel.isLastOperationByOperatorPushed()) {
+          yield new BigDecimal(displayNumber).subtract(
+              new BigDecimal(resultPanel.getBeforeNumber()));
+        } else {
+          yield new BigDecimal(resultPanel.getBeforeNumber()).subtract(
+              new BigDecimal(displayNumber));
+        }
+      }
+      case "×" ->
+          new BigDecimal(resultPanel.getBeforeNumber()).multiply(new BigDecimal(displayNumber));
+      case "÷" -> {
+        try {
+          if (resultPanel.isLastOperationByOperatorPushed()) {
+            yield new BigDecimal(displayNumber).divide(
+                new BigDecimal(resultPanel.getBeforeNumber()),
+                30,
+                RoundingMode.HALF_UP);
+          } else {
+            yield new BigDecimal(resultPanel.getBeforeNumber()).divide(
+                new BigDecimal(displayNumber),
+                30,
+                RoundingMode.HALF_UP);
+          }
+        } catch (ArithmeticException ex) {
+          throw new ArithmeticException("0除算");
+        }
+      }
+      default -> new BigDecimal(
+          "9999999999999999999999999999999999999999999999999999999999999999999999");
+    };
     return bigDecimalResult.stripTrailingZeros().toPlainString();
   }
 }
